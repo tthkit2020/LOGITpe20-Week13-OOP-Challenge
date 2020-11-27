77bdfb3484a1e0f46e7caa300426b9536bd71233
Julia Aasmaa <my_e_10@hotmail.com>
2020-11-27 10:32:08 +0200

    commit

---
+++ CarChallenge/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace CarChallenge
+{
+    class Program
+    {
+        class Car
+        {
+            string mark;
+            string model;
+            string color;
+            string regNumber;
+            int fuelTank;
+            int odo;
+
+            public Car(string _mark, string _model, string _color, string _regNumber)
+            {
+                mark = _mark;
+                model = _model;
+                color = _color;
+
+                if (_regNumber.Length != 6)
+                {
+                    regNumber = "udefined";
+                }
+                else
+                {
+                    regNumber = _regNumber;
+                }
+
+                fuelTank = 60;
+                odo = 0;
+            }
+
+            public string Model
+            {
+                get { return model; }
+            }
+
+            public string Mark
+            {
+                get { return mark; }
+            }
+
+            public string Color
+            {
+                get { return color; }
+            }
+
+            public string RegNumber
+            {
+                get { return regNumber; }
+            }
+
+            public int FuelTank
+            {
+                get { return fuelTank; }
+            }
+
+            public int Odo
+            {
+                get { return odo; }
+            }
+
+            public void Drive()
+            {
+                odo += 100;
+                fuelTank -= 5;
+            }
+
+            public void ShowCarDetails()
+            {
+                Console.WriteLine($"Model: {model};\n Mark: {mark};\n Color: {color};\n RegNumber: {regNumber}; \n Fuel: {fuelTank};\n Odo: {odo};");
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Car newCar = new Car("toyota", "supra", "red", "1234ABC");
+
+            while(newCar.FuelTank != 0)
+            {
+                newCar.Drive();
+            }
+
+            Console.WriteLine("The ride is over!");
+            newCar.ShowCarDetails();
+
+        }
+    }
+}
---
+++ OOPChallenges/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace OOPChallenges
+{
+    class Program
+    {
+        class Dog
+        {
+            string name;
+            int spots;
+            double happiness;
+
+            public Dog(string _name, int _spots)
+            {
+                name = _name;
+                spots = _spots;
+                happiness = 0;
+                Console.WriteLine($"The dog {name} has been created.");
+            }
+
+            public double Happiness
+            {
+                get { return happiness; }
+            }
+            public void Barks()
+            {
+                Console.WriteLine("Woof-woof");
+                happiness += 0.2;
+            }
+
+            public void Wiggle()
+            {
+                Console.WriteLine("Wiggle-wiggle");
+            }
+
+            public void Rename(string newName)
+            {
+                name = newName;
+            }
+
+            public void ShowDogData()
+            {
+                Console.WriteLine($"Name: {name}; Spots: {spots}; Happy: {happiness};");
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Dog newDog = new Dog("Koer", 10);
+            
+            Console.WriteLine($"The level of happinness: {newDog.Happiness}");
+
+            while(newDog.Happiness != 1)
+            {
+                newDog.Barks();
+            }
+
+            newDog.Wiggle();
+
+            Console.WriteLine("Name the dog: ");
+            string newDogName = Console.ReadLine();
+            newDog.Rename(newDogName);
+            newDog.ShowDogData();
+
+        }
+    }
+}
